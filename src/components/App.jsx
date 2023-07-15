648ca4e91c29a1db88ed9f62b63e5dc91a2698a0
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-07-15 13:06:12 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { Form, Button, List, ListItem } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label htmlFor="">
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <Button>Add contact</Button>
+        </Form>
+        <p>Contacts</p>
+        <List>
+          <ListItem></ListItem>
+        </List>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App.styled.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Input = styled.input``;
+
+export const Title = styled.h1``;
+
+export const List = styled.ul``;
+
+export const ListItem = styled.ul`
+  list-style-type: style type none;
+`;
+
+export const ListTitle = styled.p``;
+
+export const Button = styled.button`
+  width: 90px;
+  padding: 3px;
+  cursor: pointer;
+`;
