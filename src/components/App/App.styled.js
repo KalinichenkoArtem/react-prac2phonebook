81ce3baaa186efb96ece92427fa83e82a2cfdf59
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-07-15 22:37:14 +0300

    1

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,25 +1,9 @@
 import styled from '@emotion/styled';
 
-export const Form = styled.form`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 10px;
-`;
-
-export const Input = styled.input``;
-
-export const Title = styled.h1``;
-
-export const List = styled.ul``;
-
-export const ListItem = styled.ul`
-  list-style-type: style type none;
-`;
-
-export const ListTitle = styled.p``;
-
-export const Button = styled.button`
-  width: 90px;
-  padding: 3px;
-  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
 `;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,64 +1,63 @@
 import { Component } from 'react';
-
 import PropTypes from 'prop-types';
-import { Form, Button } from 'components/App/App.styled';
+import { Form, Label, Input, Button } from './ContactForm.styled';
 
 class ContactForm extends Component {
   state = {
+    contacts: [],
     name: '',
     number: '',
   };
 
-  handleChange = evt => {
-    const { name, value } = evt;
-    this.setState({ [name]: value });
+  onInputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
   };
 
-  onSubmit = e => {
-    e.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
+
     this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
     this.setState({ name: '', number: '' });
   };
 
   render() {
     const { name, number } = this.state;
+
     return (
-      <>
-        <Form onSubmit={this.handleSubmit}>
-          <label htmlFor="">
-            Name
-            <input
-              value={name}
-              onChange={this.handleChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label htmlFor="">
-            Number
-            <input
-              onChange={this.handleChange}
-              value={number}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <Button>Add contact</Button>
-        </Form>
-        <ul>
-          <li>
-            <p>
-              {this.name}:{this.number}
-            </p>
-          </li>
-        </ul>
-      </>
+      <Form onSubmit={this.handleSubmit}>
+        <Label>
+          Name
+          <Input
+            onChange={this.onInputChange}
+            value={name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          Number
+          <Input
+            onChange={this.onInputChange}
+            value={number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button>Add contact</Button>
+      </Form>
     );
   }
 }
